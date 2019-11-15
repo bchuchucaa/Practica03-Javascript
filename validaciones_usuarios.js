3d37da3ad3ac559bdb76a6550907aceb62ca3f1b
ISRAEL CHUCHUCA <israelchuchuca@MacBook-Pro-de-ISRAEL.local>
2019-11-14 21:30:55 -0500

    Validacion cedula

--- validaciones_usuarios.js
+++ validaciones_usuarios.js
@@ -1,3 +1,4 @@
+
 function validarCamposObligatorios()
 
 {
@@ -8,15 +9,185 @@ function validarCamposObligatorios()
 			if(elemento.id =='cedula'){
 				document.getElementById('mensajeCedula').innerHTML='<br>la cedula esta vacia';
 			}
-		elemento.style.border ='1px red solid';
-		elemento.className='error';
-		bandera=false;
+			else if(elemento.id =='nombres'){
+				document.getElementById('mensajeNombres').innerHTML='<br>No ah ingresado los nombres';
+			}else if(elemento.id == 'apellidos'){
+				document.getElementById('mensajeApellidos').innerHTML='<br>No ah ingresado los apellidos';
+			}else if(elemento.id == 'fecha_nac'){
+				document.getElementById('mensajeFecha').innerHTML='<br>Fecha no ingresada';
+			}else if(elemento.id == 'direccion'){
+				document.getElementById('mensajeDireccion').innerHTML='<br>Fecha no ingresada';
+			}else if(elemento.id == 'correo'){
+				document.getElementById('mensajeCorreo').innerHTML='<br>Correo no ingresado';
+			}else if(elemento.id == 'password'){
+				document.getElementById('mensajePassword').innerHTML='<br>Contrasena no ingresada';
+			}else if(elemento.id == 'telefonos'){
+				document.getElementById('mensajeTelefono').innerHTML='<br>Telefono no ingresado';
+			}
+			elemento.style.border ='1px red solid';
+			elemento.className='error';
+			bandera=false;
 		}
 	}
 	if(!bandera){
 		alert('Error revisa los comentarios');
 		return false;
 	}else{
-		return true;
+		if(validarCedula()==false){
+			alert('datos ingresados incorrectamente');
+			return false;
+		}else if (validar_clave()==false){
+			alert('La contrasena no cumple los requisitos')
+			return false;
+		}else if(validarEmail()==false){
+			alert('correo no valido');
+		}else{
+			alert('Registro exitoso');
+			return true;
+		}
+
+	}
+
+}
+
+
+function soloNumeros(e)
+{
+	var key = window.Event ? e.which : e.keyCode
+	return ((key >= 48 && key <= 57) || (key==8))
+	
+}
+
+
+
+function soloLetras(e){
+	key = e.keyCode || e.which;
+	tecla = String.fromCharCode(key).toLowerCase();
+	letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
+	especiales = "8-37-39-46";
+
+	tecla_especial = false
+	for(var i in especiales){
+		if(key == especiales[i]){
+			tecla_especial = true;
+			break;
+		}
+	}
+
+	if(letras.indexOf(tecla)==-1 && !tecla_especial){
+		return false;
 	}
 }
+function validarTelefono(elemento){
+	if (elemento.value.length <11) {
+		return false;
+	}else{
+		elemento.value=elemento.value.substring(0,elemento.value.length-1);
+	}
+
+}
+
+
+function validar(){
+	let input = document.getElementById('fecha_nac').value;
+	var reg = /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/;
+	if (input.match(reg)) {
+		console.log("formato correcto de la fecha");
+	}else {
+		console.log("Please enter dd/mm/yyyy");
+		alert('Fecha ingresada incorrectamente');
+	}
+}
+
+
+function validarCedula() {
+  console.log();
+  var cad = document.getElementById("cedula").value.trim();
+  var total = 0;
+  var longitud = cad.length;
+  var longcheck = longitud - 1;
+
+  if (cad !== "" && longitud === 10) {
+    for (i = 0; i < longcheck; i++) {
+      if (i % 2 === 0) {
+        var aux = cad.charAt(i) * 2;
+        if (aux > 9) aux -= 9;
+        total += aux;
+      } else {
+        total += parseInt(cad.charAt(i)); // parseInt o concatenará en lugar de sumar
+      }
+    }
+
+    total = total % 10 ? 10 - total % 10 : 0;
+
+    if (cad.charAt(longitud - 1) == total) {
+      console.log("La cedula" + " es valida");
+
+    } else {
+      alert("Error: La cedula" + " es invalida");
+
+    }
+  }
+}
+
+function validarEmail(){
+	var cadena = document.getElementById('correo').value,
+    separador = "@", // un espacio en blanco
+    arregloDeSubCadenas = cadena.split(separador);
+    if((arregloDeSubCadenas[1]==='est.ups.edu.ec' || arregloDeSubCadenas[1]==='ups.edu.ec') && arregloDeSubCadenas[0].length > 2  ){
+    	console.log('correo valido');
+    	return true;
+    	
+    }else{
+
+    	console.log('correo invalido');
+    	return false;
+    }
+
+    console.log(arregloDeSubCadenas);
+}
+
+
+
+function validar_clave()
+{
+	let contrasenna= document.getElementById('password').value;
+	console.log(contrasenna)
+	
+	if(contrasenna.length > 7)
+	{		
+		var mayuscula = false;
+		var minuscula = false;
+		var numero = false;
+		var caracter_raro = false;
+
+		for(var i = 0;i<contrasenna.length;i++)
+		{
+			if(contrasenna.charCodeAt(i) >= 65 && contrasenna.charCodeAt(i) <= 90)
+			{
+				mayuscula = true;
+			}
+			else if(contrasenna.charCodeAt(i) >= 97 && contrasenna.charCodeAt(i) <= 122)
+			{
+				minuscula = true;
+			}
+			else if(contrasenna.charCodeAt(i) >= 48 && contrasenna.charCodeAt(i) <= 57)
+			{
+				numero = true;
+			}
+			else
+			{
+				caracter_raro = true;
+			}
+		}
+		if(mayuscula == true && minuscula == true && caracter_raro == true && numero == true)
+		{
+			
+			return true;
+		}
+	}
+	console.log('Clave no cumple con los requisitos');
+	return false;
+	
+}
+
